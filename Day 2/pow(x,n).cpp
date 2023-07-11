3c1d9c889b04ecbe02c98931c200445426456b8c
Umang Verma <vermaumang7@gmail.com>
2023-07-11 22:43:21 +0530

    Create pow(x,n).cpp

---
+++ Day 2/pow(x,n).cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+         if(n==0) return 1;
+    if(n==1) return x;
+    if(n>0)
+    {
+        if(n%2==0)
+        {
+            return myPow(x*x,n/2);
+        } else {
+          return x * myPow(x * x, n / 2);
+        }
+    }
+    else
+    {
+        n=abs(n);
+        x=1/x;
+        if(n%2==0)
+        {
+            return myPow(x*x,n/2);
+        }
+        else return x*myPow(x*x,n/2);
+    }
+    }
+};
+   
